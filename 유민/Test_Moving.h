4bdc3451b34d7ec097c76684056028969c642ad4
tracer12 <vendetta12@naver.com>
2023-11-07 16:33:15 +0900

    Create Test_Moving.h

---
+++ 유민/Test_Moving.h
@@ -0,0 +1,16 @@
+#ifndef __TESTMOVING_H__
+#define __TESTMOVING_H__
+
+#include <windows.h>
+#include "calculatorDST.h"
+
+void ShowBlock();
+void DeleteBlock();
+int ShiftLeft();
+int ShiftRight();
+int BlockUp();
+int BlockDown();
+int NpcMoving();
+
+
+#endif
